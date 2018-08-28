ef742109b5c7d98f4824f677f00a9e3d1d1365b2
Michael Henriksen <mhenriks@redhat.com>
2018-08-28 14:02:55 -0400

    proxy implemented again and disabled signature checking

--- pkg/controller/upload-controller.go
+++ pkg/controller/upload-controller.go
@@ -72,7 +72,8 @@ type UploadController struct {
 	caKeyPair                                 *triple.KeyPair
 }
 
-func getUploadResourceNames(pvcName string) string {
+// GetUploadResourceName returns the name given to upload services/pods
+func GetUploadResourceName(pvcName string) string {
 	return "cdi-upload-" + pvcName
 }
 
@@ -297,7 +298,7 @@ func (c *UploadController) syncHandler(key string) error {
 		return errors.Wrapf(err, "error getting PVC %s", key)
 	}
 
-	resourceName := getUploadResourceNames(pvc.Name)
+	resourceName := GetUploadResourceName(pvc.Name)
 
 	if _, exists := pvc.ObjectMeta.Annotations[AnnUploadRequest]; !exists {
 		// delete everything
